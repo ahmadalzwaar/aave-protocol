d6c682b1d11e411bef153bda0da07cebb2425821
ahmadalzwaar <64903845+ahmadalzwaar@users.noreply.github.com>
2021-03-01 00:32:27 +0300

    Update LendingPoolAddressesProvider.sol

--- contracts/configuration/LendingPoolAddressesProvider.sol
+++ contracts/configuration/LendingPoolAddressesProvider.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.0;
 
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 import "../libraries/openzeppelin-upgradeability/InitializableAdminUpgradeabilityProxy.sol";
 
 import "./AddressStorage.sol";
